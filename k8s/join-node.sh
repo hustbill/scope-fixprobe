0b43e2a60d0037b19c99a53665bc561ba9d0ac8d
hustbill <billyzhang2010@gmail.com>
2017-04-06 13:21:52 -0700

    move status report to devops-hw-network virtualization

---
+++ k8s/join-node.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+kubeadm join --token d6dfbb.0716debafb61cf65 10.145.240.104:6443
+
--- weave-scope-demo/install/scope-http-statistics-plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-# If you want to use the Scope HTTP Statistics plugin in an already set up Kubernetes cluster with Weave Scope running on it, you just need to run:
-
-kubectl create -f https://github.com/weaveworks-plugins/scope-http-statistics/tree/master/deployments/k8s-http-statistics.yaml
-
-
-
