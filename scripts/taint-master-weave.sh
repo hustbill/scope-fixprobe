61960af1bd27e35b862cee73f115cb31f40fc297
Hua Zhang <hua.zhang1@huawei.com>
2017-04-17 14:44:33 -0700

    rename taint-master-weave.sh

--- scripts/taint-master-weave.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp /etc/kubernetes/admin.conf  ~/admin.conf
-#kubectl create -f https://git.io/weave-kube
-kubectl apply -f https://git.io/weave-kube-1.6
-kubectl taint nodes --all node-role.kubernetes.io/master- # single node!
